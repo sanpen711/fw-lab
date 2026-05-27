9b4eb63adfefa93127396324abbb4a6f58b40e4d
sanpen711 <yspxl1202@gmail.com>
2026-05-27 09:28:40 +0800

    Bump mobile app cache for comment layout

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fw-mobile-app-square-detail-comments-1';
+const CACHE_NAME = 'fw-mobile-app-square-comment-layout-1';
 const APP_SHELL = [
   '/app/install.html',
   '/app/index.html',
