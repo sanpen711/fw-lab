54bba1d44d9a6371ad7ec22c462ee9153a98e8fe
sanpen711 <yspxl1202@gmail.com>
2026-06-08 17:39:38 +0800

    Refresh mobile-only researcher label cache

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fw-mobile-app-auth-choice-20260608-1';
+const CACHE_NAME = 'fw-mobile-app-mobile-only-label-20260608-1';
 const APP_BASE = new URL('./', self.location.href).pathname;
 const SITE_BASE = APP_BASE.replace(/app\/?$/, '');
 const appPath = path => APP_BASE + path;
@@ -55,7 +55,7 @@ function injectAppTweaks(html){
   next = next.replace(/\.\/nav\.js\?v=[^"']+/g, './nav.js?v=mobile-reload-hash-20260608-1');
   next = next.replace(/\.\/modules-init\.js\?v=[^"']+/g, './modules-init.js?v=mobile-comment-thread-20260608-1');
   if(next.indexOf('fw-researcher-id-label.js') < 0){
-    next = next.replace('</body>', tweakTag('../assets/fw-researcher-id-label.js?v=researcher-id-label-20260608-1'));
+    next = next.replace('</body>', tweakTag('../assets/fw-researcher-id-label.js?v=mobile-only-label-20260608-1'));
   }
   if(next.indexOf('echo-enhance.js') < 0){
     next = next.replace('</body>', tweakTag('./echo-enhance.js?v=mobile-echo-enhance-20260529-2'));
