3456d71d9e32675df44a9e79075f0cb85fa7bab4
sanpen711 <yspxl1202@gmail.com>
2026-06-09 11:01:16 +0800

    Bump PWA cache for persistent buddy unread badge

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fw-mobile-app-buddy-core-20260609-2';
+const CACHE_NAME = 'fw-mobile-app-buddy-core-20260609-3';
 const APP_BASE = new URL('./', self.location.href).pathname;
 const SITE_BASE = APP_BASE.replace(/app\/?$/, '');
 const appPath = path => APP_BASE + path;
@@ -70,9 +70,9 @@ function injectAppTweaks(html){
     next = next.replace('</body>', tweakTag('./bird-tweaks.js?v=mobile-bird-toggle-20260528-1'));
   }
   if(next.indexOf('buddy-read-tweaks.js') < 0){
-    next = next.replace('</body>', tweakTag('./buddy-read-tweaks.js?v=mobile-buddy-read-20260609-2'));
+    next = next.replace('</body>', tweakTag('./buddy-read-tweaks.js?v=mobile-buddy-read-20260609-3'));
   }else{
-    next = next.replace(/\.\/buddy-read-tweaks\.js\?v=[^"']+/g, './buddy-read-tweaks.js?v=mobile-buddy-read-20260609-2');
+    next = next.replace(/\.\/buddy-read-tweaks\.js\?v=[^"']+/g, './buddy-read-tweaks.js?v=mobile-buddy-read-20260609-3');
   }
   if(next.indexOf('buddy-chat-tweaks.js') < 0){
     next = next.replace('</body>', tweakTag('./buddy-chat-tweaks.js?v=mobile-buddy-chat-20260528-1'));
