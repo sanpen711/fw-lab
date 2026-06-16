04851df8c6ba69d3c2518647be7d997bba12eda0
sanpen711 <yspxl1202@gmail.com>
2026-06-16 14:24:21 +0800

    Cache mobile media cache script in PWA shell

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fw-mobile-app-pwa-stable-20260616-1';
+const CACHE_NAME = 'fw-mobile-app-pwa-stable-20260616-2';
 const APP_BASE = new URL('./', self.location.href).pathname;
 const SITE_BASE = APP_BASE.replace(/app\/?$/, '');
 const appPath = path => APP_BASE + path;
@@ -49,6 +49,7 @@ const APP_SHELL = [
   appPath('mobile-priority-fixes.js'),
   appPath('mobile-swipe-back.js'),
   appPath('mobile-transitions.js'),
+  appPath('mobile-media-cache.js'),
   appPath('manifest.webmanifest')
 ];
 
