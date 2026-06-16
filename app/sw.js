c929f5162bc1d3416dd0388a5e801858fd9c8737
sanpen711 <yspxl1202@gmail.com>
2026-06-16 15:35:46 +0800

    Refresh PWA cache after feed ensureLoaded cache fix

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fw-mobile-app-pwa-stable-20260616-4';
+const CACHE_NAME = 'fw-mobile-app-pwa-stable-20260616-5';
 const APP_BASE = new URL('./', self.location.href).pathname;
 const SITE_BASE = APP_BASE.replace(/app\/?$/, '');
 const appPath = path => APP_BASE + path;
