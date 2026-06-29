3e122d64de10059cb04027fbe190f0448e1e5421
sanpen711 <yspxl1202@gmail.com>
2026-06-29 13:46:30 +0800

    Refresh PWA cache for avatar repair

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fw-mobile-app-pwa-stable-20260629-echo-avatar-1';
+const CACHE_NAME = 'fw-mobile-app-pwa-stable-20260629-avatar-repair-1';
 const APP_BASE = new URL('./', self.location.href).pathname;
 const SITE_BASE = APP_BASE.replace(/app\/?$/, '');
 const appPath = path => APP_BASE + path;
