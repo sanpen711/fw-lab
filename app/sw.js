120a43c5b51cc911bdf73b341d2000b515a47923
sanpen711 <yspxl1202@gmail.com>
2026-06-29 11:40:19 +0800

    Refresh PWA cache for direct swipe back

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fw-mobile-app-pwa-stable-20260629-sticker-1';
+const CACHE_NAME = 'fw-mobile-app-pwa-stable-20260629-swipe-direct-1';
 const APP_BASE = new URL('./', self.location.href).pathname;
 const SITE_BASE = APP_BASE.replace(/app\/?$/, '');
 const appPath = path => APP_BASE + path;
