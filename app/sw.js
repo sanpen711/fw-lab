5a5dce9f4df278fa3a3467c3a239ef04452639bf
sanpen711 <yspxl1202@gmail.com>
2026-06-29 16:44:40 +0800

    Refresh PWA cache for quiet sync guard

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fw-mobile-app-pwa-stable-20260629-buddy-return-1';
+const CACHE_NAME = 'fw-mobile-app-pwa-stable-20260629-quiet-sync-1';
 const APP_BASE = new URL('./', self.location.href).pathname;
 const SITE_BASE = APP_BASE.replace(/app\/?$/, '');
 const appPath = path => APP_BASE + path;
