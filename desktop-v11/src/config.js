c297b20aa0a0aee05cfadd41328baa34db0970be
sanpen711 <yspxl1202@gmail.com>
2026-08-19 11:47:45 +0800

    release: bump Windows desktop to 1.1.10

--- desktop-v11/src/config.js
+++ desktop-v11/src/config.js
@@ -1,4 +1,4 @@
-export const APP_VERSION = '1.1.9';
+export const APP_VERSION = '1.1.10';
 export const SUPABASE_URL = 'https://ekbovsmxbiplhyrzxoyw.supabase.co';
 export const SUPABASE_ANON_KEY = 'sb_publishable_I1sZnPGEoozkjWcKTXjauQ_qSBsx5Bh';
 export const ACCOUNT_CACHE_KEY = 'fw:desktop:v11:account';
